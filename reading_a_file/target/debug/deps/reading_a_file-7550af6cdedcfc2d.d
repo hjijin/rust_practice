691a4fcedba4d385a7f14363968aae219a92d092
Jim Huang <hjijin.gz@gmail.com>
2022-05-09 16:35:29 +0800

    delete useless files.

--- reading_a_file/target/debug/deps/reading_a_file-7550af6cdedcfc2d.d
+++
@@ -1,5 +0,0 @@
-/Users/hjj/Workspace/github/rust_practice/reading_a_file/target/debug/deps/reading_a_file-7550af6cdedcfc2d: src/main.rs
-
-/Users/hjj/Workspace/github/rust_practice/reading_a_file/target/debug/deps/reading_a_file-7550af6cdedcfc2d.d: src/main.rs
-
-src/main.rs:
--- reading_a_file/target/debug/reading_a_file.d
+++
@@ -1,1 +0,0 @@
-/Users/hjj/Workspace/github/rust_practice/reading_a_file/target/debug/reading_a_file: /Users/hjj/Workspace/github/rust_practice/reading_a_file/src/main.rs
